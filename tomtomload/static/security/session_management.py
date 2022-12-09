e144853e5b1fa543c65439f3a3d2c2cfa945137c
YeePingCyber <67046533+YeePingCyber@users.noreply.github.com>
2022-12-09 20:16:57 +0800

    Session Management

--- tomtomload/static/security/session_management.py
+++ tomtomload/static/security/session_management.py
@@ -1,6 +1,13 @@
 from flask_paranoid import Paranoid
+import jwt
 
 class TTLSession(Paranoid):
     def __init__(self, app=None):
-        super().__init__(app)
+        self.__flask_paranoid = super().__init__(app)
     
+    def write_token_to_session(self, token):
+        return super().write_token_to_session(token)
+    
+    def get_token(self, username, password):
+        if username == "" and password == "":
+            return 
