5a3067288d4f95845f2d9c9e3fa3d5e2f561f2fe
smizrx <mizukigkkm@gmail.com>
2023-01-17 17:27:54 +0800

    sign up validation

---
+++ tomtomload/static/classes/form.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, validators, PasswordField
+from wtforms.validators import DataRequired, ValidationError
+
+class Login(FlaskForm):
+    email = StringField([validators.Length(min=1, max=30), validators.DataRequired()], render_kw={"placeholder": "Email"})
+    password = PasswordField([validators.Length(min=8, max=15), validators.Regexp(regex="(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}"),
+                        validators.DataRequired()], render_kw={"placeholder": "Password"})
+
+    
+
+class SignUp(FlaskForm):
+    email = StringField([validators.Length(min=1, max=30), validators.DataRequired()], render_kw={"placeholder": "Email"})
+    password = PasswordField([validators.Length(min=8, max=15), validators.Regexp(regex="(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}"),
+                        validators.DataRequired()], render_kw={"placeholder": "Password"})
+
+class Forget(FlaskForm):
+    email = StringField([validators.Length(min=1, max=30), validators.DataRequired()], render_kw={"placeholder": "Email"})
