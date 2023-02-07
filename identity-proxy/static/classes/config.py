1e382793bf127fdb1d00b59f72e709737a01e390
YeePingCyber <67046533+YeePingCyber@users.noreply.github.com>
2023-02-08 01:35:57 +0800

    y does my api no work

--- identity-proxy/static/classes/config.py
+++ identity-proxy/static/classes/config.py
@@ -19,7 +19,7 @@ class Constants:
     DOMAIN: str = "127.0.0.1" if DEBUG_MODE else "tomtomload.com"
 
     CALLBACK_URL: str = "https://127.0.0.1:8080/callback" if DEBUG_MODE else "https://tomtomload.com/callback"
-    API_CALLBACK_URL: str = "https://127.0.0.1:8080/api/callback" if DEBUG_MODE else "https://tomtomload.com/api/callback"
+    API_ROUTE_URL: str = "https://127.0.0.1:5000/api" if DEBUG_MODE else "https://tomtomload.com/api"
     ADMIN_URL: str = "https://127.0.0.1:5000/admin" if DEBUG_MODE else "https://www.tomtomload.com/admin"
 
     # -----------------  GOOGLE CLOUD ----------------- #
