50c187d51b28043612366a99bba4a3f0dbc42613
YeePingCyber <67046533+YeePingCyber@users.noreply.github.com>
2023-02-13 02:22:12 +0800

    from the other side

--- tomtomload/routes/api.py
+++ tomtomload/routes/api.py
@@ -11,6 +11,7 @@ from static.classes.storage import GoogleCloudStorage
 from static.security.secure_data import GoogleCloudKeyManagement, Encryption
 from static.security.session_management import TTLSession
 from static.security.ttl_limiter import TTL_Limiter
+from static.classes.unique_id import UniqueID
 
 from ast import literal_eval
 
@@ -127,21 +128,6 @@ def api_view_admins():
 
         return_dict = {"id":decoded_dict['google_id'], "name":decoded_dict['name'], "email":decoded_dict['email'], "role":decoded_dict['role']}
         return jsonify(admin_details=return_dict),200
-    
-    
-
-
-# @api.route("/view_admin/<regex('[0-9]{21}'):id>", methods=["GET"])
-# @ttl_redirect_user
-# @ttl_jwt_authentication
-# @ttlLimiter.limit_user(limit_value="10/day")
-# def api_view_admin(id):
-#     if decoded_jwt()["role"] == "SuperAdmins":
-#         pass
-#     else:
-#         return jsonify(Error="Unauthorized access"),401
-
-#     return jsonify(message="Work in progress"),200
 
 
 @api.route("/edit_admin/<regex('[0-9]{21}'):id>", methods=["PUT"])
@@ -191,18 +177,6 @@ def api_delete_admin(id):
 
 
 # ------ Post API ------
-@api.route("/create_post", methods=["POST"])
-@ttl_redirect_user
-@ttl_jwt_authentication
-@ttlLimiter.limit_user(limit_value="10/day")
-def api_create_post():
-    if ttl_check_user_agent():
-        return jsonify(message="Web Create post"),200
-        # return redirect(url_for("authorised_user.post"))
-    
-    return jsonify(message="Work in progress"),200
-
-
 @api.route("/view_post", methods=["GET"])
 @ttl_redirect_user
 @ttl_jwt_authentication
@@ -260,7 +234,7 @@ def api_view_posts():
 
 
 @api.route("/view_post/<regex('[0-9a-f]{32}'):id>", methods=["GET"])
-# @ttl_redirect_user
+@ttl_redirect_user
 @ttl_jwt_authentication
 @ttlLimiter.limit_user(limit_value="10/day")
 def api_view_post(id):
@@ -289,8 +263,6 @@ def api_view_post(id):
             
             # -----------------  START OF DECRYPTION ----------------- #
             
-            # sasd = b'\n$\x00\x88B\xcc\xb8\x815M\xa7\x03y\x8caE\xd2\x1c\\\x99\x85\x04\r\x16\x12M\xb4\xe0"\xc7\xe1\x04\t\x12Cx\xc1\xef\x12\xbc\x01*\xb9\x01\n\x14\n\x0c\xca\x8e\x87~\x94\x15=1I\xa1\x89G\x10\xe6\xcd\xfe\x9e\x02\x12\x87\x01\n\x7f\xb8\x8e)\x17_\xba\xa9\xb7\xd7x\x7f\x99p^\xb2y\xb0.\\[\xe7\xd9\xd0n\xe6z\x00\xae\x98\x89hj\x10\xfay\x8d\xc8Ew=&sp\xf4\xe5*\x86?w_-i\t\xdcv\x97\x1b\x9a\x03\x1cJrY\x95/j\xf2\xb4c\x1a\xb5\xf9\xb7i\xd8v\xf8\xa5\x17\\Q\n:+67\xfdI\xa9\x82o\xa4\x82d\xd9\xef(\xd7\xad\xc2pqxGz\x97\x8a\xd1\\Lm6\xac\x19\xf8\x9f&\xe8\xdfVMg\x87\xc4\x1a\xcf\xdf\x10\x9f\x97\x9b\x84\x0f\x1a\x17\n\x10Z<\x87--\xc9\xde\xc20\x124\xe3v\xcf \x1a\x10\xf4\x99\x90H'
-            # print("\n\n")
             decrypted_content = encryption.decrypt_symmetric(
                 project_id = CONSTANTS.GOOGLE_PROJECT_ID,
                 location_id = CONSTANTS.GOOGLE_LOCATION_ID,
@@ -317,12 +289,39 @@ def api_update_post(id):
     return jsonify(message="Work in progress"),200
 
 
-@api.route("/delete_post/<regex('[0-9a-f]{32}'):id>", methods=["DELETE"])
+@api.route("/delete_post/<regex('[0-9a-f]{32}'):id>", methods=["GET"])
 @ttl_redirect_user
 @ttl_jwt_authentication
 @ttlLimiter.limit_user(limit_value="10/day")
 def api_delete_post(id):
-    return jsonify(message="Work in progress"),200
+    if ttl_check_user_agent() and len(id) == 32:
+        return redirect(f"{CONSTANTS.ADMIN_URL}/admin/posts/delete/{id}")
+        # {CONSTANTS.ADMIN_URL}
+    else:
+
+        return jsonify(message="This mode have not been unlocked."),200
+        # decoded_dict = decoded_jwt()
+
+        # if not ttl_check_user_agent():
+        #     name = decoded_dict["name"]
+
+        # elif ttl_check_user_agent() and ttlSession.verfiy_Ptoken("TTLAuthenticatedUserName"):
+        #     name = ttlSession.get_data_from_session("TTLAuthenticatedUserName", data=True)
+
+        # else:
+        #     return jsonify(Error="Unauthorized Access"),401
+
+
+        # storage.delete_blob(
+        #     bucket_name = CONSTANTS.STORAGE_BUCKET_NAME,
+        #     blob_name = decoded_dict["role"] + "/" + name + "/post/" + id + ".json",
+        # )
+
+        # temp_post_path = os.path.join(CONSTANTS.TTL_CONFIG_FOLDER, "post", id)
+        # temp_post_path = temp_post_path + ".json"
+
+        # if os.path.isfile(temp_post_path):
+        #     os.remove(temp_post_path)
 
 
 # ------ Media API ------
@@ -331,7 +330,10 @@ def api_delete_post(id):
 @ttl_jwt_authentication
 @ttlLimiter.limit_user(limit_value="10/day")
 def api_create_media():
-    return jsonify(message="Work in progress"),200
+    media_id = UniqueID()
+
+    return redirect("authorised_user.media_upload", id=media_id)
+
 
 
 @api.route("/view_media", methods=["GET"])
@@ -402,14 +404,6 @@ def api_view_media(id):
     else:
         return jsonify(Error="Unauthorized Access"),401
 
-    # path = os.path.join(CONSTANTS.TTL_CONFIG_FOLDER, "media" , id)
-    # path = path + ".png"
-
-    # storage.download_blob(
-    #     bucket_name = CONSTANTS.STORAGE_BUCKET_NAME,
-    #     source_blob_name = decoded_jwt["role"] + "/" + ttlSession.get_data_from_session("TTLAuthenticatedUserName", data=True) + "/media/" + media_id + ".png",
-    #     destination_file_name = path
-    # )
 
     if ttl_check_user_agent():
         url = storage.generate_download_signed_url(
@@ -419,14 +413,6 @@ def api_view_media(id):
         return jsonify(url=url),200
 
     return jsonify(message="Work in progress"),200
-
-
-# @api.route("/update_media/<regex('[0-9a-f]{32}'):id>", methods=["PUT"])
-# @ttl_redirect_user
-# @ttl_jwt_authentication
-# @ttlLimiter.limit_user(limit_value="10/day")
-# def api_update_media(id):
-#     return jsonify(message="Work in progress"),200
 
 
 @api.route("/delete_media/<regex('[0-9a-f]{32}'):id>", methods=["DELETE"])
